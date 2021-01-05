10f59593d56e8fe242f64e466d3bda7ec1b8c419
muranokami <pikupiku68574.dj@gmail.com>
2021-01-05 21:50:31 +0900

    商品購入機能　５

--- spec/models/purchase_spec.rb
+++ spec/models/purchase_spec.rb
@@ -5,80 +5,87 @@ RSpec.describe UserPurchase, type: :model do
     before do
      @purchase = FactoryBot.build(:user_purchase)
     end
+     
+    describe '商品購入機能' do
+      context '商品が購入できる時' do
+        it "user_idとitem_idとpost_idとshipping_area_idとmunicipalitieとaddressとphone_numberとtokenが存在すれば登録できること" do
+          expect(@purchase).to be_valid
+        end
+      end
 
-    it "user_idとitem_idとpost_idとshipping_area_idとmunicipalitieとaddressとphone_numberとtokenが存在すれば登録できること" do
-      expect(@purchase).to be_valid
-    end
+      context '商品が購入できない時' do
+        
+        it "user_idが空では登録できないこと" do
+          @purchase.user_id = ""
+          @purchase.valid?
+          expect(@purchase.errors.full_messages).to include("User can't be blank")
+        end
 
-    it "user_idが空では登録できないこと" do
-      @purchase.user_id = ""
-      @purchase.valid?
-      expect(@purchase.errors.full_messages).to include("User can't be blank")
-    end
+        it "item_idが空では登録できないこと" do
+          @purchase.item_id = ""
+          @purchase.valid?
+          expect(@purchase.errors.full_messages).to include("Item can't be blank")
+        end
 
-    it "item_idが空では登録できないこと" do
-      @purchase.item_id = ""
-      @purchase.valid?
-      expect(@purchase.errors.full_messages).to include("Item can't be blank")
-    end
+        it "post_idが空では登録できないこと" do
+          @purchase.post_id = ""
+          @purchase.valid?
+          expect(@purchase.errors.full_messages).to include("Post can't be blank")
+        end
 
-    it "post_idが空では登録できないこと" do
-      @purchase.post_id = ""
-      @purchase.valid?
-      expect(@purchase.errors.full_messages).to include("Post can't be blank")
-    end
+        it "post_idにはハイフンが必要であること" do
+          @purchase.post_id = 1234567
+          @purchase.valid?
+          expect(@purchase.errors.full_messages).to include("Post is invalid")
+        end
 
-    it "post_idにはハイフンが必要であること" do
-      @purchase.post_id = 1234567
-      @purchase.valid?
-      expect(@purchase.errors.full_messages).to include("Post is invalid")
-    end
+        it "shipping_area_idが空では登録できないこと" do
+          @purchase.shipping_area_id = ""
+          @purchase.valid?
+          expect(@purchase.errors.full_messages).to include("Shipping area can't be blank")
+        end
 
-    it "shipping_area_idが空では登録できないこと" do
-      @purchase.shipping_area_id = ""
-      @purchase.valid?
-      expect(@purchase.errors.full_messages).to include("Shipping area can't be blank")
-    end
+        it "municipalitieが空では登録できないこと" do
+          @purchase.municipalitie = ""
+          @purchase.valid?
+          expect(@purchase.errors.full_messages).to include("Municipalitie can't be blank")
+        end
+       
+        it "addressが空では登録できないこと" do
+          @purchase.address = ""
+          @purchase.valid?
+          expect(@purchase.errors.full_messages).to include("Address can't be blank")
+        end
 
-    it "municipalitieが空では登録できないこと" do
-      @purchase.municipalitie = ""
-      @purchase.valid?
-      expect(@purchase.errors.full_messages).to include("Municipalitie can't be blank")
-    end
+        it "building_numberが空でも登録できること" do
+          @purchase.building_name = ""
+          @purchase.valid?
+        end
 
-    it "addressが空では登録できないこと" do
-      @purchase.address = ""
-      @purchase.valid?
-      expect(@purchase.errors.full_messages).to include("Address can't be blank")
-    end
+        it "phone_numberが空では登録できないこと" do
+          @purchase.phone_number = ""
+          @purchase.valid?
+          expect(@purchase.errors.full_messages).to include("Phone number can't be blank")
+        end
 
-    it "building_numberが空でも登録できること" do
-      @purchase.building_name = ""
-      @purchase.valid?
-    end
+        it "phone_numberにはハイフンが不要" do
+          @purchase.phone_number = 123-4567-34
+          @purchase.valid?
+          expect(@purchase.errors.full_messages).to include("Phone number is invalid")
+        end
 
-    it "phone_numberが空では登録できないこと" do
-      @purchase.phone_number = ""
-      @purchase.valid?
-      expect(@purchase.errors.full_messages).to include("Phone number can't be blank")
-    end
+        it "phone_numberは11桁以内であること" do
+          @purchase.phone_number = 123456789012
+          @purchase.valid?
+         expect(@purchase.errors.full_messages).to include("Phone number is invalid")
+        end
 
-    it "phone_numberにはハイフンが不要" do
-      @purchase.phone_number = 123-4567-34
-      @purchase.valid?
-      expect(@purchase.errors.full_messages).to include("Phone number is invalid")
-    end
-
-    it "phone_numberは11桁以内であること" do
-      @purchase.phone_number = 123456789012
-      @purchase.valid?
-      expect(@purchase.errors.full_messages).to include("Phone number is invalid")
-    end
-
-    it "tokenが空では登録できないこと" do
-      @purchase.token = nil
-      @purchase.valid?
-      expect(@purchase.errors.full_messages).to include("Token can't be blank")
+        it "tokenが空では登録できないこと" do
+          @purchase.token = nil
+          @purchase.valid?
+          expect(@purchase.errors.full_messages).to include("Token can't be blank")
+        end
+      end
     end
   end
 end
